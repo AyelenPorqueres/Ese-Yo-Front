2cd8e10094ed57c526175a59006eb0b1f304ef79
Guada-Chojo <gchojohernandez@gmail.com>
2024-11-11 18:04:49 -0300

    Cards acciones inicio

--- front/src/app/components/banner/banner.tsx
+++ front/src/app/components/banner/banner.tsx
@@ -5,11 +5,11 @@ import "@/app/components/banner/banner.css"
 export function Banner() {
     return (
         <>
-            <div className="bannerContainer">
+            <div className="bannerContainer text-black">
                 <div className="tituloBanner">La diversión es por aquí</div>
                 <div className="subtituloBanner">Creemos en el poder transformador del juego y el deporte, y trabajamos para hacerlo accesible a todos.</div>
                 <button className="botonDonar">DONÁ AHORA</button>
-                <img className="bannerImagen" src="/banner.png" alt="Imagen del banner"></img>
+                <img className="bannerImagen" src="./images/banner.png" alt="Imagen del banner"></img>
             </div>
         </>
     )
