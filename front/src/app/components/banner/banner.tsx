4754a168d5db3047b79f008e582f9ffe320f3ec1
Juan1Suarez <juansuarez60249@gmail.com>
2024-11-11 14:22:25 -0300

    Banner

---
+++ front/src/app/components/banner/banner.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from "react";
+import "@/app/components/banner/banner.css"
+
+export function Banner() {
+    return (
+        <>
+            <div className="bannerContainer">
+                <div className="tituloBanner">La diversión es por aquí</div>
+                <div className="subtituloBanner">Creemos en el poder transformador del juego y el deporte, y trabajamos para hacerlo accesible a todos.</div>
+                <button className="botonDonar">DONÁ AHORA</button>
+                <img className="bannerImagen" src="/banner.png" alt="Imagen del banner"></img>
+            </div>
+        </>
+    )
+}
