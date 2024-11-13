f0dba2af90c3fccaa86ec73270e54a662eff0a91
ThiagoSchiaffino <thiagoschiaffinodechia@gmail.com>
2024-11-13 20:41:55 -0300

    comoSurgio

--- front/src/app/(pages)/quienesSomos/page.tsx
+++ front/src/app/(pages)/quienesSomos/page.tsx
@@ -1,3 +1,9 @@
+import { ComoSurgio } from "@/app/components/comoSurgio/comoSurgio";
+
 export default function QuienesSomos () {
-    
+    return(
+        <div className="bg-white">
+           <ComoSurgio/>
+        </div>
+        );
 }
