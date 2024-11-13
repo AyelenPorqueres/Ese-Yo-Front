a2d9612520d29fdcb817df3b8a4fa6f5933d8b7a
Guada-Chojo <gchojohernandez@gmail.com>
2024-11-13 20:31:10 -0300

    Accordions mision, mision y valores

--- front/src/app/(pages)/quienesSomos/page.tsx
+++ front/src/app/(pages)/quienesSomos/page.tsx
@@ -1,3 +1,11 @@
+import { Accordions } from "@/app/components/accordions/accordions";
+import NavBar from "@/app/components/nav/nav";
+
 export default function QuienesSomos () {
-    
+    return(
+        <div className="bg-white">
+            <NavBar />
+            <Accordions />
+        </div>
+    );
 }
