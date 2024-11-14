90486b8788fc6750f825a275a9236a5d4498a9e3
Juan1Suarez <juansuarez60249@gmail.com>
2024-11-14 19:34:14 -0300

    fundación y banner a tailwind

--- front/src/app/components/banner/banner.tsx
+++ front/src/app/components/banner/banner.tsx
@@ -1,16 +1,15 @@
 'use client'
 import React from "react";
-import "@/app/components/banner/banner.css"
 
 export function Banner() {
     return (
         <>
-            <div className="bannerContainer text-black">
-                <div className="tituloBanner">La diversión es por aquí</div>
-                <div className="subtituloBanner">Creemos en el poder transformador del juego y el deporte, y trabajamos para hacerlo accesible a todos.</div>
-                <button className="botonDonar">DONÁ AHORA</button>
-                <img className="bannerImagen" src="./images/banner.png" alt="Imagen del banner"></img>
+            <div className="w-full h-[520px] flex flex-col items-center gap-5 text-center relative overflow-hidden pt-7 text-black flex-wrap">
+                <div className="text-[2.625em] font-bold font-[cursive] z-10">La diversión es por aquí</div>
+                <div className="text-[1.5625em] w-auto max-w-[90%] md:max-w-[710px] z-10">Creemos en el poder transformador del juego y el deporte, y trabajamos para hacerlo accesible a todos.</div>
+                <button className="rounded-lg border border-gray-200 bg-[#f2cc0f] text-white h-10 w-44 text-[1.25em] z-10 hover:bg-white hover:text-black">DONÁ AHORA</button>
+                <img className="w-full h-[520px] object-cover absolute top-0 left-0" src="./images/banner.png" alt="Imagen del banner" />
             </div>
         </>
-    )
+    );
 }
--- front/src/app/components/fundacion/fundacion.tsx
+++ front/src/app/components/fundacion/fundacion.tsx
@@ -1,17 +1,16 @@
 'use client'
 import React from "react";
-import "@/app/components/fundacion/fundacion.css"
 
 export function Fundacion() {
     return (
         <>
-            <div className="fundacionContainer">
-                <img className="fundacionImagen" src='./images/fundacion.png' alt="Imagen de la fundacion"></img>
-                <div className="fundacionTexto">
-                    <a className="fundacionTitulo" href="/quienesSomos#laFundacion">FUNDACIÓN ESE-YO</a>
-                    <div className="fundacionDescripcion">La FUNDACIÓN ESE-YO es una entidad comprometida con el bienestar y el desarrollo de la infancia. Nuestra misión es clara: queremos que los niños y niñas sean felices, y sabemos que la felicidad se encuentra en el juego. Por eso, nos dedicamos a facilitar y enriquecer ambientes lúdicos, donde el aprendizaje y la diversión van de la mano. Creemos firmemente en el poder transformador del juego y del deporte, y trabajamos para hacerlo accesible para todos.</div>
+            <div className="flex flex-col items-center relative text-black">
+                <img className="w-full h-auto max-h-[520px]" src="./images/fundacion.png" alt="Imagen de la fundacion"/>
+                <div className="bg-white -mt-[20em] p-9 rounded-xl mr-11 text-center border-2 border-gray-300">
+                    <a className="text-[2.0625em] font-bold hover:underline cursor-pointer" href="/quienesSomos#laFundacion">FUNDACIÓN ESE-YO</a>
+                    <div className="text-[1.125em] max-w-[53.25em] mt-2 pl-20 pr-20">La FUNDACIÓN ESE-YO es una entidad comprometida con el bienestar y el desarrollo de la infancia. Nuestra misión es clara: queremos que los niños y niñas sean felices, y sabemos que la felicidad se encuentra en el juego. Por eso, nos dedicamos a facilitar y enriquecer ambientes lúdicos, donde el aprendizaje y la diversión van de la mano. Creemos firmemente en el poder transformador del juego y del deporte, y trabajamos para hacerlo accesible para todos.</div>
                 </div>
             </div>
         </>
-    )
+    );
 }
