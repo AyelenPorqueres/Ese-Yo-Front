ee24b1ed361f91199ca12128857881cedb81510e
ThiagoSchiaffino <thiagoschiaffinodechia@gmail.com>
2024-11-20 14:54:01 -0300

    arreglo como surgio

--- front/src/app/components/comoSurgio/comoSurgio.tsx
+++ front/src/app/components/comoSurgio/comoSurgio.tsx
@@ -5,7 +5,7 @@ export function ComoSurgio() {
   return (
     <div>
     <div className="contenedor border-y-4 border-green-300">
-    <h1 className="titulo w-60 h-20 text-4xl text-center ml-[800px] text-[#000000] relative top-5 mt-[20px]"> Como Surgio</h1>
+    <h1 className="titulo w-60 h-20 text-4xl text-center ml-[800px] text-[#000000] relative top-5 mt-[20px]"> Como Surgió</h1>
     <p className="texto text-[#000000] w-[1600px] ml-[150px] text-2xl mb-[40px]  bg-white">Surgió en un bar de Buenos Aires. Estábamos comiendo con unos amigos. Como suele suceder en la ciudad, en repetidas ocasiones se nos acercaron personas para preguntarnos si podríamos ayudarlos. Ya sea comprando alguno de los productos que ofrecían o compartiendo una ración de nuestro plato. Conversando con los demás miembros del proyecto, que estaba iniciándose, nos dimos cuenta que esta situación no solo tenía lugar en la capital sino también en el interior. Mismas necesidades, distinta latitud. <br></br><br></br>
 
 Entre la precoz imaginación de todos los posibles futuros y la intensidad momentánea de nuestro compromiso social, surgió la pregunta: ¿Qué hacemos nosotros por el otro? Ese otro. Ese otro que no contó, quizá, con las posibilidades y privilegios que nosotros gozamos. Ese otro que no dispuso de la protección de un techo, que no sintió amor, que no recibió alimento. Ese otro a quien educaron miles y miles de no. Nosotros saciando todas nuestras necesidades y ese otro mendigando para llenar su barriga.<br></br><br></br>
